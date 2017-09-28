e47e84cba31af8a7e8b0c4f7e9027cfcf15773ee
SohailChamadia <sohailyc@gmail.com>
2017-09-28 10:18:50 +0530

    FileChooser Dialog Interface Successful

--- app/src/main/java/com/example/sohail/intratorr/MainActivity.java
+++ app/src/main/java/com/example/sohail/intratorr/MainActivity.java
@@ -1,30 +1,54 @@
 package com.example.sohail.intratorr;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.transition.Transition;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private static final int REQUEST_WRITE_PERMISSION = 786;
+
+    private Boolean kill = false;
+    private void changeFragment(Fragment fm){
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content, fm);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            kill = false;
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_download:
+                    changeFragment(new Download());
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_history:
+                    changeFragment(new History());
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_files:
+                    changeFragment(new Files());
                     return true;
             }
             return false;
@@ -33,13 +57,38 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public void onBackPressed() {
+        if(kill) {
+            finish();
+        }
+        else{
+            Toast.makeText(this,"Press again to exit",Toast.LENGTH_SHORT).show();
+            kill = true;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            changeFragment(new History());
+        }
+        else{
+            finish();
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+        }
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        requestPermission();
     }
-
 }
